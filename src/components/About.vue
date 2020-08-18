e02fc01dd710339fdc1bf409a985728fd61070f0
lionel <mohamedaitrami@gmail.com>
2020-08-18 22:37:46 +0100

    :pencil2: fixing typo in about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
       <img alt="logo" class="img-fluid logo" src="../assets/logo-to.png"/>
       <h1 class="h1-responsive mt-3 mb-2">About Us</h1>
       <br>
-      <p style="font-size:120%">At TwitterOversight, we believe there is a better way to saty in touch with what's happening now in the topics that interests you. We're obsessively passionate about it, and our mission is to help you achieve it by providing top tweets in those topics and analyzing their sentiments.</p>
+      <p style="font-size:120%">At TwitterOversight, we believe there is a better way to stay in touch with what's happening now in the topics that interests you. We're obsessively passionate about it, and our mission is to help you achieve it by providing top tweets in those topics and analyzing their sentiments.</p>
     </div>
   </section>
 </template>
