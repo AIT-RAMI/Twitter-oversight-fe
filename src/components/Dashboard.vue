bab7767c034763fa187a566fcf16e29d60be444e
lionel <mohamedaitrami@gmail.com>
2020-08-18 18:54:45 +0100

    :sparkles: Adding Linechart

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
   <section id="dashboard">
+    <!-- Search query
     <mdb-card class="mb-4">
       <mdb-card-body class="d-sm-flex justify-content-between">
         <h4 class="mb-sm-0 pt-2">
@@ -11,28 +12,47 @@
         </form>
       </mdb-card-body>
     </mdb-card>
+    -->
     <section class="mt-lg-5">
       <mdb-row>
-        <mdb-col xl="3" md="6" class="mb-r">
+        <mdb-col md="12" lg="4" class="mb-4">
           <mdb-card cascade class="cascading-admin-card">
             <div class="admin-up">
-              <mdb-icon icon="money-bill-alt" far class="primary-color"/>
               <div class="data">
-                <p>SALES</p>
+                <p>Number Of Tweets</p>
                 <h4>
-                  <strong>$2000</strong>
+                  <strong>0.877k</strong>
                 </h4>
               </div>
             </div>
-            <mdb-card-body>
-              <div class="progress">
-                <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="25" class="progress-bar bg-primary" role="progressbar"
-                  style="width: 25%"></div>
-              </div>
-              <mdb-card-text>Better than last week (25%)</mdb-card-text>
-            </mdb-card-body>
           </mdb-card>
         </mdb-col>
+        <mdb-col md="12" lg="4" class="mb-4">
+          <mdb-card cascade class="cascading-admin-card">
+            <div class="admin-up">
+              <div class="data">
+                <p>Positive Sentiments</p>
+                <h4>
+                  <strong>135</strong>
+                </h4>
+              </div>
+            </div>
+          </mdb-card>
+        </mdb-col>
+        <mdb-col md="12" lg="4" class="mb-4">
+          <mdb-card cascade class="cascading-admin-card">
+            <div class="admin-up">
+              <div class="data">
+                <p>Negative Sentiments</p>
+                <h4>
+                  <strong>243</strong>
+                </h4>
+              </div>
+            </div>
+          </mdb-card>
+        </mdb-col>
+
+        <!--
         <mdb-col xl="3" md="6" class="mb-r">
           <mdb-card cascade class="cascading-admin-card">
             <div class="admin-up">
@@ -90,6 +110,20 @@
                   style="width: 25%"></div>
               </div>
               <mdb-card-text>Better than last week (25%)</mdb-card-text>
+            </mdb-card-body>
+          </mdb-card>
+        </mdb-col>
+        -->
+      </mdb-row>
+    </section>
+    <section>
+      <mdb-row class="mt-5">
+        <mdb-col md="12" class="mb-4">
+          <mdb-card>
+            <mdb-card-body>
+                    <div style="display: block">
+                      <line-chart/>
+                    </div>
             </mdb-card-body>
           </mdb-card>
         </mdb-col>
@@ -537,18 +571,19 @@
 </template>
 
 <script>
-import { mdbRow, mdbCol, mdbBtn, mdbCard, mdbCardBody, mdbCardHeader, mdbCardText, mdbIcon, mdbTbl, mdbBarChart, mdbPieChart, mdbLineChart, mdbRadarChart, mdbDoughnutChart, mdbListGroup, mdbListGroupItem, mdbBadge, mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter } from 'mdbvue'
+import LineChart from '@/components/LineChart.vue';
+import { mdbRow, mdbCol, mdbBtn, mdbCard, mdbCardBody, mdbCardHeader, mdbIcon, mdbTbl, mdbBarChart, mdbPieChart, mdbLineChart, mdbRadarChart, mdbDoughnutChart, mdbListGroup, mdbListGroupItem, mdbBadge, mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter } from 'mdbvue'
 
 export default {
   name: 'Dashboard',
   components: {
+    'line-chart': LineChart,
     mdbRow,
     mdbCol,
     mdbBtn,
     mdbCard,
     mdbCardBody,
     mdbCardHeader,
-    mdbCardText,
     mdbIcon,
     mdbTbl,
     mdbBarChart,
@@ -581,6 +616,27 @@ export default {
       showFluidModalLeft: false,
       showFluidModalTop: false,
       showFluidModalBottom: false,
+      lineChartData: {
+        labels: ['10 Aug', '11 Aug', '12 Aug', '13 Aug', '14 Aug', '15 Aug', '16 Aug'],
+        datasets: [
+          {
+            label: '#1',
+            data: [12, 39, 3, 50, 2, 32, 84],
+            backgroundColor: 'rgba(245, 74, 85, 0.5)',
+            borderWidth: 1
+          }, {
+            label: '#2',
+            data: [56, 24, 5, 16, 45, 24, 8],
+            backgroundColor: 'rgba(90, 173, 246, 0.5)',
+            borderWidth: 1
+          }, {
+            label: '#3',
+            data: [12, 25, 54, 3, 15, 44, 3],
+            backgroundColor: 'rgba(245, 192, 50, 0.5)',
+            borderWidth: 1
+          }
+        ]
+      },
       barChartData: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
         datasets: [
@@ -629,7 +685,6 @@ export default {
         datasets: [
           {
             data: [300, 50, 100, 40, 120, 24, 52],
-            backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360', '#ac64ad'],
             hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774', '#da92db']
           }
         ]
@@ -637,26 +692,6 @@ export default {
       pieChartOptions: {
         responsive: true,
         maintainAspectRatio: false
-      },
-      lineChartData: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        datasets: [
-          {
-            label: '#1',
-            backgroundColor: 'rgba(245, 74, 85, 0.5)',
-            data: [65, 59, 80, 81, 56, 55, 40]
-          },
-          {
-            label: '#2',
-            backgroundColor: 'rgba(90, 173, 246, 0.5)',
-            data: [12, 42, 121, 56, 24, 12, 2]
-          },
-          {
-            label: '#3',
-            backgroundColor: 'rgba(245, 192, 50, 0.5)',
-            data: [2, 123, 154, 76, 54, 23, 5]
-          }
-        ]
       },
       lineChartOptions: {
         responsive: true,
@@ -722,7 +757,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .cascading-admin-card {
-  margin: 20px 0;
+  margin: 0px 0;
 }
 .cascading-admin-card .admin-up {
   margin-left: 4%;
@@ -735,18 +770,21 @@ export default {
   padding: 1.7rem;
   font-size: 2rem;
   color: #fff;
-  text-align: left;
+  text-align: center;
   margin-right: 1rem;
   border-radius: 3px;
 }
 .cascading-admin-card .admin-up .data {
-  float: right;
+  float: center;
   margin-top: 2rem;
-  text-align: right;
+  text-align: center;
 }
 .admin-up .data p {
-  color: #999999;
-  font-size: 12px;
+  color: #000000;
+  font-size: 20px;
+}
+.admin-up .data h4 {
+  margin-bottom: 20px
 }
 .classic-admin-card .card-body {
   color: #fff;
---
+++ src/components/LineChart.vue
@@ -0,0 +1,55 @@
+<script>
+	import {Line} from "vue-chartjs";
+	export default {
+		extends: Line,
+		props: {
+			hashtag: String
+		},
+		data: () => ({
+			
+			chartdata: {
+			labels: ['13 Aug', '14 Aug', '15 Aug', '16 Aug'],
+			datasets: [{
+						label: "Positive Sentiment",
+						backgroundColor: "#27ae60",
+						borderColor: "#27ae60",
+						fill: false,
+						data: [70, 160, 52, 260]
+					},
+					{
+						label: "Negative Sentiment",
+						backgroundColor: "#f87979",
+						borderColor: "#f87979",
+						fill: false,
+						data: [15, 45, 49, 350]
+					}
+				]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						},
+						gridLines: {
+							display: true
+						}
+					}],
+					xAxes: [{
+						gridLines: {
+							display: false
+						}
+					}]
+				},
+				legend: {
+					display: true
+				},
+				responsive: true,
+				maintainAspectRatio: false
+			}
+		}),
+		mounted() {
+			this.renderChart(this.chartdata, this.options)
+		}
+	}
+</script>
